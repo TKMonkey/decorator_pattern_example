489aa7732100a9e866eef63e8788defdede85ac6
Juan E Quintero R <juan@57blocks.com>
2021-07-27 07:52:01 -0500

    feat: add ControllerWithLocalDatabase that receives an IDatabase

---
+++ src/pre_decorator/common_interface/controller_local_database.ts
@@ -0,0 +1,14 @@
+export class ControllerWithLocalDatabase{
+
+    constructor(private database: IDatabase){}
+    
+    async call(): Promise<void>{
+        console.log('En el controlador que recibe IDatabase Y le mandé LocalDatabase');
+        await this.database.create({id: "1", name: "Facundo"});
+        const user1 = await this.database.read("1");
+        await this.database.update({name: "Roberto"}, "1");
+        await this.database.delete("1");
+
+    }
+
+}
