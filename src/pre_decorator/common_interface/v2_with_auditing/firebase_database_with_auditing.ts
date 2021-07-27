1981f1358321e60422889d05af91bf78426f13d0
Juan E Quintero R <juan@57blocks.com>
2021-07-27 07:51:03 -0500

    feat: implement FirebaseDatabase with common interface and with auditing

---
+++ src/pre_decorator/common_interface/v2_with_auditing/firebase_database_with_auditing.ts
@@ -0,0 +1,24 @@
+export class FirebaseDatabase implements IDatabase{
+
+    async create(data: object): Promise<void> {
+        console.log('AUDITING create FirebaseDatabase implementing IDatabase');
+        console.log('create FirebaseDatabase:', data );
+    }
+
+    async read(id: string): Promise<object>{
+        console.log('read FirebaseDatabase id:', id);
+
+        return {id};
+    }
+
+    async update(data: object, id: string): Promise<void>{
+        console.log('AUDITING update FirebaseDatabase implementing IDatabase');
+        console.log('update FirebaseDatabase, data:', data, ' id:', id);
+    }
+
+    async delete(id: string): Promise<void>{
+        console.log('AUDITING delete FirebaseDatabase implementing IDatabase');
+        console.log('delete FirebaseDatabase id:', id);
+    }
+
+}
