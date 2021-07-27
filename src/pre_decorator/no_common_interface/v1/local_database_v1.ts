b4476ff5d50e81259923abf4decf85cbe1db21a1
Juan E Quintero R <juan@57blocks.com>
2021-07-27 07:47:19 -0500

    feat: add LocalDatabase with no common interface

---
+++ src/pre_decorator/no_common_interface/v1/local_database_v1.ts
@@ -0,0 +1,24 @@
+export class LocalDatabase{
+
+    async create(data: object): Promise<void> {
+        console.log('AUDITING create LocalDatabase');
+        console.log('create LocalDatabase:', data );
+    }
+
+    async read(id: string): Promise<object>{
+        console.log('read LocalDatabase id:', id);
+
+        return {id};
+    }
+
+    async update(data: object, id: string): Promise<void>{
+        console.log('AUDITING update LocalDatabase');
+        console.log('update LocalDatabase, data:', data, ' id:', id);
+    }
+
+    async delete(id: string): Promise<void>{
+        console.log('AUDITING delete LocalDatabase');
+        console.log('delete LocalDatabase id:', id);
+    }
+
+}
