f7417f1f399b148e9b8bd9117bfd96d1b3231f59
Juan E Quintero R <juan@57blocks.com>
2021-07-27 07:50:38 -0500

    feat: implement FirebaseDatabase with common interface

---
+++ src/pre_decorator/common_interface/v1/firebase_database_v1.ts
@@ -0,0 +1,21 @@
+export class FirebaseDatabase implements IDatabase{
+
+    async create(data: object): Promise<void> {
+        console.log('create FirebaseDatabase implementing IDatabase:', data );
+    }
+
+    async read(id: string): Promise<object>{
+        console.log('read FirebaseDatabase implementing IDatabase id:', id);
+
+        return {id};
+    }
+
+    async update(data: object, id: string): Promise<void>{
+        console.log('update FirebaseDatabase implementing IDatabase, data:', data, ' id:', id);
+    }
+
+    async delete(id: string): Promise<void>{
+        console.log('delete FirebaseDatabase implementing IDatabase id:', id);
+    }
+
+}
